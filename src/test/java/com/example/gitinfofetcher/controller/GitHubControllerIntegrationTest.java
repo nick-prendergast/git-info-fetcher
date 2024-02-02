e499310c5532fee80c8d8bdded5dbfa1eab22790
nick prendergast <nick_prendergast@hotmail.com>
2024-02-02 15:26:11 +0100

    fix wiremock tests

--- src/test/java/com/example/gitinfofetcher/controller/GitHubControllerIntegrationTest.java
+++ src/test/java/com/example/gitinfofetcher/controller/GitHubControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import com.example.gitinfofetcher.config.WebFluxErrorHandlingConfig;
 import com.example.gitinfofetcher.service.GitHubService;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
@@ -23,6 +22,9 @@ import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.anyRequestedFor;
 
 @WebFluxTest(controllers = GitHubController.class)
 @Import({GitHubService.class, WebClientTestConfig.class, WebFluxErrorHandlingConfig.class})
@@ -32,32 +34,49 @@ public class GitHubControllerIntegrationTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @BeforeEach
-    public void setUp() {
-        setupStubForRepositoryBranches();
-        setupStubForUserRepositories();
+    @Test
+    public void shouldReturnNotFoundForNonExistentUserRepositories() {
+        stubForWhenUserDoesNotExist();
+
+        webTestClient.get().uri("/api/github/users/nonexistentuser/repos")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.status").isEqualTo(404)
+                .jsonPath("$.Message").isEqualTo("Not Found");
+
+        verify(WireMock.getRequestedFor(urlPathMatching("/users/nonexistentuser/repos")));
     }
 
-    private void setupStubForRepositoryBranches() {
-        String[] repositories = {"git-consortium", "hello-worId", "Hello-World", "octocat.github.io", "Spoon-Knife", "test-repo1"};
+    private static void stubForWhenUserDoesNotExist() {
+        stubFor(get(urlPathMatching("/users/nonexistentuser/repos"))
+                .willReturn(aResponse()
+                        .withStatus(404)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{ \"message\": \"Not Found\", \"documentation_url\": " +
+                                "\"https://docs.github.com/rest/repos/repos#list-repositories-for-a-user\" }")));
+    }
+
+    @Test
+    public void shouldReturnRepositoriesForExistingUser() {
+        String[] repositories = {"git-consortium", "hello-worId", "Hello-World", "octocat.github.io",
+                "Spoon-Knife", "test-repo1"};
         for (String repo : repositories) {
             stubForRepositoryBranches(repo, repo + ".json");
         }
-    }
 
-    private void setupStubForUserRepositories() {
-        stubForUserRepositoryFound();
-        stubForUserRepositoryNotFound();
-        stubForUserRepositoryWithXmlHeader();
-        stubForUserRepositoryNotExisting();
-    }
+        stubForUserRepos();
 
-    private static void stubForUserRepositoryNotExisting() {
-        stubFor(WireMock.get(WireMock.urlPathMatching("/users/some-user/repos"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("[]")));
+        String jsonContentResponse = readJsonFromFile("__files/json/response/octocat-response.json");
+        webTestClient.get().uri("/api/github/users/octocat/repos")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .json(jsonContentResponse);
+
+        verify(WireMock.getRequestedFor(urlPathMatching("/users/octocat/repos")));
     }
 
     private void stubForRepositoryBranches(String repositoryName, String jsonFileName) {
@@ -68,7 +87,7 @@ public class GitHubControllerIntegrationTest {
                         .withBodyFile("json/branches/" + jsonFileName)));
     }
 
-    private void stubForUserRepositoryFound() {
+    private static void stubForUserRepos() {
         stubFor(WireMock.get(WireMock.urlPathMatching("/users/octocat/repos"))
                 .willReturn(aResponse()
                         .withStatus(200)
@@ -76,15 +95,23 @@ public class GitHubControllerIntegrationTest {
                         .withBodyFile("json/repos/repos.json")));
     }
 
-    private void stubForUserRepositoryNotFound() {
-        stubFor(get(urlPathMatching("/users/nonexistentuser/repos"))
-                .willReturn(aResponse()
-                        .withStatus(404)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("{ \"message\": \"Not Found\", \"documentation_url\": \"https://docs.github.com/rest/repos/repos#list-repositories-for-a-user\" }")));
+
+    @Test
+    public void shouldReturnNotAcceptableForXmlHeaderRequest() {
+        stubForRequestWithXmlHeader();
+
+        webTestClient.get().uri("/api/github/users/xmltest/repos")
+                .accept(MediaType.APPLICATION_XML)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_ACCEPTABLE)
+                .expectBody()
+                .jsonPath("$.status").isEqualTo(HttpStatus.NOT_ACCEPTABLE.value())
+                .jsonPath("$.Message").isEqualTo("The requested media type is not supported");
+
+        verify(0, getRequestedFor(urlPathMatching("/users/.+/repos")));
     }
 
-    private void stubForUserRepositoryWithXmlHeader() {
+    private static void stubForRequestWithXmlHeader() {
         stubFor(get(urlPathMatching("/api/github/users/xmltest/repos"))
                 .withHeader("Accept", equalTo("application/xml"))
                 .willReturn(aResponse()
@@ -94,45 +121,24 @@ public class GitHubControllerIntegrationTest {
     }
 
     @Test
-    public void shouldReturnNotFoundForNonExistentUserRepositories() {
-        webTestClient.get().uri("/api/github/users/nonexistentuser/repos")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isNotFound()
-                .expectBody()
-                .jsonPath("$.status").isEqualTo(404)
-                .jsonPath("$.Message").isEqualTo("Not Found");
-    }
-
-    @Test
-    public void shouldReturnRepositoriesForExistingUser() {
-        String jsonContent = readJsonFromFile("__files/json/response/octocat-response.json");
-        webTestClient.get().uri("/api/github/users/octocat/repos")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .json(jsonContent);
-    }
-
-    @Test
-    public void shouldReturnNotAcceptableForXmlHeaderRequest() {
-        webTestClient.get().uri("/api/github/users/xmltest/repos")
-                .accept(MediaType.APPLICATION_XML)
-                .exchange()
-                .expectStatus().isEqualTo(HttpStatus.NOT_ACCEPTABLE)
-                .expectBody()
-                .jsonPath("$.status").isEqualTo(HttpStatus.NOT_ACCEPTABLE.value())
-                .jsonPath("$.Message").isEqualTo("The requested media type is not supported");
-    }
-
-    @Test
     public void userExistsButHasNoRepositoriesTest() {
+        stubForUserWithNoRepos();
+
         webTestClient.get().uri("/api/github/users/some-user/repos")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody().json("[]");
+
+        verify(WireMock.getRequestedFor(urlPathMatching("/users/some-user/repos")));
+    }
+
+    private static void stubForUserWithNoRepos() {
+        stubFor(WireMock.get(WireMock.urlPathMatching("/users/some-user/repos"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[]")));
     }
 
     @Test
@@ -141,6 +147,8 @@ public class GitHubControllerIntegrationTest {
                 .exchange()
                 .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
                 .expectBody().isEmpty();
+
+        verify(0, anyRequestedFor(urlPathMatching("/api/github/users/.+/repos")));
     }
 
     private String readJsonFromFile(String path) {
